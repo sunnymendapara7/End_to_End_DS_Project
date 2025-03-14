cb202d2db522b0ac54a9a891b5745ae6f2077c5a
sunnymendapara7 <sunnymendapara09@gmail.com>
2025-03-14 14:34:02 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-# from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 # from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 # from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
@@ -27,15 +27,15 @@ except Exception as e:
         raise e
 
 
-# STAGE_NAME = "Data Transformation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataTransformationTrainingPipeline()
-#    data_ingestion.initiate_data_transformation()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 # STAGE_NAME = "Model Trainer stage"
---
+++ src/datascience/pipeline/data_transformation_pipeline.py
@@ -0,0 +1,28 @@
+from src.datascience.config.configuration import ConfigurationManager
+from src.datascience.components.data_transformation import DataTransformation
+from src.datascience import logger
+from sklearn.model_selection import train_test_split
+
+
+STAGE_NAME="Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_transformation(self):
+        config=ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.initiate_data_transformation()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
