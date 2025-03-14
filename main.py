4778db02a097d361c2b45213c50a1fa3cc39dfc7
sunnymendapara7 <sunnymendapara09@gmail.com>
2025-03-14 11:02:59 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
-# from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 # from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 # from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 # from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
@@ -16,15 +16,15 @@ except Exception as e:
         raise e
 
 
-# STAGE_NAME = "Data Validation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataValidationTrainingPipeline()
-#    data_ingestion.initiate_data_validation()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 # STAGE_NAME = "Data Transformation stage"
